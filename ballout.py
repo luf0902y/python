ab53c7552c35c44e869a71b1bbc3a717a19508ee
HankLu <luf0902y@gmail.com>
2016-05-26 22:45:19 +0800

    Update ballout.py

--- ballout.py
+++ ballout.py
@@ -2,7 +2,7 @@ import random
 filename = 'namelist.txt'
 filehandle = open(filename)
 st=[]
-t = random.randrange(0,19)
+t = random.randrange(0,21)
 for line in filehandle:
 	line = line.split()
 	st.append(line[2])
