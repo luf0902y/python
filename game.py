ae93e8e02ac00775248c1a4dc749b9d864df275d
HankLu <luf0902y@gmail.com>
2016-06-17 02:18:15 +0800

    Update game.py

--- game.py
+++ game.py
@@ -1,39 +1,42 @@
-# -*- coding: utf-8 -*-
 import random
-import os
-print ('來猜猜數字')
-ST = []
 t=0
-while t<4: #先設定題目四個數字，且不能重複
-	num = str(random.randrange(0,10))
-	if not (num in ST):
-		ST.append(num)
-		t+=1
+ans=[]
 time=1
-while True:
-	inp = input('輸入四個數字\t')
-	A=0
-	B=0
-	if "STOP" in inp:  #輸入"STOP"則放棄，公布答案
-		print('停止遊戲，答案是：')
-		print ("".join(str(n) for n in ST))
+while t<4:
+	n = random.randrange(0,10)
+	n = str(n)
+	if n not in ans:
+		ans.append(n)
+		t+=1
+	else:
+		continue
+ANS = "".join(ans)
+print(ANS)
+while True :
+	inp = input('猜一猜')
+	if inp == "STOP":
+		print ('遊戲結束！\n答案是：',ANS)
 		break
 	else:
 		try:
-			int(inp)  #確保輸入數字
-			if "".join(str(n) for n in ST) == inp:  #當和答案輸入相同時，就對了！
-				print ('太棒了 答對囉！')
-				print ('總共花了',time,'次')
-				break
-			elif len(inp) != 4:	#確保輸入的是「四個」數字
-				print ('確實打四個數字！')
+			int(inp)
+			if len(inp) > 4 :
+				print('請輸入四個數字！')
+				continue
 			else:
-				for num in range(4):  #顯示幾Ａ幾Ｂ
-					if ST[num] == inp[num]:	 #數字對，位置也對
-						A+=1
-					if inp[num] in ST: #輸入的數字有在答案裡面
-						B+=1
-				print (A,'A',B-A,'b')  #數字對，位置錯的部分是(B-A)
-			time+=1
+				if inp == ANS:
+					print ('真厲害！答對了！')
+					print ('共用了',time,'次')
+					break
+				else:
+					A=0
+					B=0
+					for p in range(0,4):
+						if inp[p] == ANS[p]:
+							A+=1
+						if inp[p] in ANS:
+							B+=1
+				print (A,'A',B-A,'B')
 		except:
-			print('別亂打')
+			print('請輸入四個數字！')
+	time+=1
